87052ad40dad1cf48588d5fab48d454355116a2a
Mugerwa Joseph <mugsjsp21@gmail.com>
2023-11-14 20:02:11 +0300

    Application files
    Application files.

---
+++ models.js
@@ -0,0 +1,14 @@
+//Remember to import in mongoose
+
+
+//create the citizen schema here with the appropriate data types
+
+
+//create the title schema here wit the right fields
+
+
+//convert the schemas into models from here
+
+
+//export the models as modules from here.
+
---
+++ server.js
@@ -0,0 +1,19 @@
+//this is where your express sever is created and managed
+
+//do not forget to import the necessary modules,
+
+
+//do not forget to connect to your mongoDb
+
+
+
+//do not forget to set up routes to serve out the html file
+
+// Do not forget to handle your form posts for the relevant forms
+
+// Do not forget to handle form gets for the necessary data to display on the user interface.
+
+
+//Do not forget to catch all the erros that may arise during the process of parsing data from one form to another.
+
+// do not forget to create a port for your application and serving out the appliacation via the port created 
