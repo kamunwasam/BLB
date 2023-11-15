a494eef0e0e489400a82a25577769fe04ad4e090
samuel wa kwatekwa <skamunua@gmail.com>
2023-11-15 17:25:26 +0300

    BLB

--- models.js
+++ models.js
@@ -1,14 +1,29 @@
 //Remember to import in mongoose
-
+const mongoose =require('mongoose')
 
 //create the citizen schema here with the appropriate data types
+const citizenSchema = new mongoose.Schema({
+  name:String,
+  dob:String,
+  Fname:String,
+  Mname:String,
+  gender:String,
+  Bg:String
+})
 
 
 //create the title schema here wit the right fields
-
-
+const titleSchema = new mongoose.Schema({
+  ownerName: String,
+  landLocation:String,
+  landSize:String,
+  xCoordinate:String,
+  yCoordinate:String,
+  titleNumber:String
+})
 //convert the schemas into models from here
-
+const citizenModel = mongoose.model('models', citizenSchema)
 
 //export the models as modules from here.
-
+module.exports = citizenModel
+module.exports =  titleSchema
--- server.js
+++ server.js
@@ -1,19 +1,49 @@
 //this is where your express sever is created and managed
-
+const express = require ('express')
+const app = express()
 //do not forget to import the necessary modules,
-
-
+const mongoose = require('mongoose')
+const path = require('path')
+const citizenModel = require('models')
+const titleSchema = require('model')
 //do not forget to connect to your mongoDb
 
+mongoose.connection = ('mongodb+srv://kamunwa:30090Sam@cluster0.xm2mttk.mongodb.net/BLB?retryWrites=true&w=majority',{
+  useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open',function(){
+  console.log('connected to the db BLB')
+})
 
 //do not forget to set up routes to serve out the html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // Do not forget to handle your form posts for the relevant forms
-
+app.post('/index', (req, res) => {
+  const { name, dob,Fname,Mname,gender,Bg  } = req.body;
+  const newCitizen = new citizenModel({ name, dob,Fname,Mname,gender,Bg });
+  
+  newCitizen.save()
+    .then(() => {
+      res.send('Sign-up successful!');
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send('Sign-up failed. Please try again.');
+    });
+});
 // Do not forget to handle form gets for the necessary data to display on the user interface.
-
 
 //Do not forget to catch all the erros that may arise during the process of parsing data from one form to another.
 
 // do not forget to create a port for your application and serving out the appliacation via the port created 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
